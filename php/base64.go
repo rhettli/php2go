8ba8260465aac153328cfdd5c09fcf572dacdaf8
Your Name <you@example.com>
2020-09-25 19:47:03 +0800

    u

--- php/base64.go
+++ php/base64.go
@@ -3,12 +3,12 @@ package php
 import "encoding/base64"
 
 // Base64Encode - Encodes data with MIME base64
-func Base64Encode(s string) string {
-	return base64.StdEncoding.EncodeToString([]byte(s))
+func Base64Encode(s []byte) string {
+	return base64.StdEncoding.EncodeToString(s)
 }
 
 // Base64Decode - Decodes data encoded with MIME base64
-func Base64Decode(s string) (string, error) {
+func Base64Decode(s string) ([]byte, error) {
 	bs, err := base64.StdEncoding.DecodeString(s)
-	return string(bs), err
+	return bs, err
 }
